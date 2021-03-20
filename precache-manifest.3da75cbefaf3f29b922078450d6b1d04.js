12ad18485d9185f4783041769873ae7618eb837b
taseentanvir <taseentanvir@gmail.com>
2021-03-20 20:55:04 +0600

    Updates

---
+++ precache-manifest.3da75cbefaf3f29b922078450d6b1d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3598f6578b7f16145112b1135ca736",
+    "url": "/react-typing-test/index.html"
+  },
+  {
+    "revision": "a4015a9e3b4f090d4ce7",
+    "url": "/react-typing-test/static/css/main.5c0da415.chunk.css"
+  },
+  {
+    "revision": "a88595f4bebcaf010080",
+    "url": "/react-typing-test/static/js/2.30c17023.chunk.js"
+  },
+  {
+    "revision": "a4015a9e3b4f090d4ce7",
+    "url": "/react-typing-test/static/js/main.75a89826.chunk.js"
+  },
+  {
+    "revision": "0a2148475b0e4d8cca2f",
+    "url": "/react-typing-test/static/js/runtime-main.912a9e48.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-typing-test/static/media/logo.25bf045c.svg"
+  }
+]);
